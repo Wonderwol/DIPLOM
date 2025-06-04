7f7f8bce43a2a436db938ad390497cb83f945716
Your Name <your-email@example.com>
2025-06-04 07:05:44 +0700

    host7

--- app.py
+++ app.py
@@ -506,10 +506,10 @@ def view_article(article_id):
     return render_template('article_detail.html', article=article)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+# Для хоста
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
